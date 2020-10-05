47cbae555968cabc87e5ff70e5bc9521f63a7fdf
Charlotte Smallwood-Smith <c.smallwood.smith@gmail.com>
2020-10-05 11:45:56 +0100

    Wrote failing test for deposit method

--- spec/BankSpec.js
+++ spec/BankSpec.js
@@ -7,5 +7,11 @@ describe("Bank", function() {
 
   it("Should return a balance", function() {
     expect(bank.balance()).toEqual(1000);
+  
+  });
+
+  it("Should deposit 1000 into account", function() {
+    bank.deposit(1000)
+    expect(bank.balance()).toEqual(1000);
   });
 });
