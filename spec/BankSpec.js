c61c2e6d1f44fe999fdcb1c5149d9e46a6c655a8
Charlotte Smallwood-Smith <c.smallwood.smith@gmail.com>
2020-10-07 16:38:25 +0100

    Edited tests to test for behaviour not state

--- spec/BankSpec.js
+++ spec/BankSpec.js
@@ -14,30 +14,36 @@ describe("Account", function() {
   });
 
   it("Deposit method should push string into statement instance variable", function() {
+    console.log = jasmine.createSpy("log");
     account.deposit(200)
-    expect(account.myStatement).toContain(`${new Date(Date.now()).toLocaleString().split(',')[0]} || 200.00 || || 200.00`)
+    account.statement();
+    expect(console.log).toHaveBeenCalledWith(`${new Date(Date.now()).toLocaleString().split(',')[0]} || 200.00 || || 200.00`)
   });
 
   it("Withdraw method should push string into statement instance variable", function() {
+    console.log = jasmine.createSpy("log");
     account.deposit(200)
     account.withdraw(50)
-    expect(account.myStatement).toContain(`${new Date(Date.now()).toLocaleString().split(',')[0]} || || 50.00 || 150.00`)
+    account.statement();
+    expect(console.log).toHaveBeenCalledWith(`${new Date(Date.now()).toLocaleString().split(',')[0]} || || 50.00 || 150.00`)
   });
 
   it("My statement array contains a list of transactions", function() {
+    console.log = jasmine.createSpy("log");
     account.deposit(400)
     account.withdraw(24)
     account.deposit(50)
-    expect(account.myStatement).toContain(`${new Date(Date.now()).toLocaleString().split(',')[0]} || 400.00 || || 400.00`)
-    expect(account.myStatement).toContain(`${new Date(Date.now()).toLocaleString().split(',')[0]} || || 24.00 || 376.00`)
-    expect(account.myStatement).toContain(`${new Date(Date.now()).toLocaleString().split(',')[0]} || 50.00 || || 426.00`)
+    account.statement();
+    expect(console.log).toHaveBeenCalledWith(`${new Date(Date.now()).toLocaleString().split(',')[0]} || 400.00 || || 400.00`)
+    expect(console.log).toHaveBeenCalledWith(`${new Date(Date.now()).toLocaleString().split(',')[0]} || || 24.00 || 376.00`)
+    expect(console.log).toHaveBeenCalledWith(`${new Date(Date.now()).toLocaleString().split(',')[0]} || 50.00 || || 426.00`)
   });
 
   it("Prints a statement list with a header to the terminal", function() {
+    console.log = jasmine.createSpy("log");
     account.deposit(400)
     account.withdraw(24)
     account.deposit(50)
-    console.log = jasmine.createSpy("log");
     account.statement();
     expect(console.log).toHaveBeenCalledWith(`date || credit || debit || balance`);
     expect(console.log).toHaveBeenCalledWith(`${new Date(Date.now()).toLocaleString().split(',')[0]} || 400.00 || || 400.00`);
@@ -46,8 +52,10 @@ describe("Account", function() {
   });
 
   it("Takes in two arguments and pushes them into an array in string format ", function(){
+    console.log = jasmine.createSpy("log");
     account.deposit(70)
-    expect(account.myStatement).toContain(`${new Date(Date.now()).toLocaleString().split(',')[0]} || 70.00 || || 70.00`)
+    account.statement();
+    expect(console.log).toHaveBeenCalledWith(`${new Date(Date.now()).toLocaleString().split(',')[0]} || 70.00 || || 70.00`)
   });
 
 });
